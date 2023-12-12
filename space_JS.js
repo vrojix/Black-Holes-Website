6f395e750155cf5f19a40ff02dea99982cbfdd76
vrojix <147489855+vrojix@users.noreply.github.com>
2023-12-12 22:40:12 +0000

    Add files via upload

---
+++ space_JS.js
@@ -0,0 +1,12 @@
+
+
+
+
+function hide_sidebar() {
+    if (document.getElementById('sidebar').hidden == true){
+        document.getElementById('sidebar').hidden = false;
+    }
+    else {
+        document.getElementById('sidebar').hidden = true;
+    }
+}
